45031e753206212e897e5229f0421a544afd4776
Samuel Febreiro <samuelfebreiro@gmail.com>
2024-01-21 12:32:41 +0100

    config.js

--- database/config.js
+++ database/config.js
@@ -4,9 +4,9 @@ const dbConnection = async() => {
     try {
         await mongoose.connect(process.env.DB_CNN, {
             // Actualmente ya no hacen falta
-            useNewUrlParser: true, 
-            useUnifiedTopology: true,
-            useCreateIndex: true
+            // useNewUrlParser: true, 
+            // useUnifiedTopology: true,
+            // useCreateIndex: true
         });
         console.log('DB Online');
     } catch (error) {
